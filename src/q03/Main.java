42261dd88c19c991010d19e809aabb2318aaeb5a
pranshuCodee <159226022+pranshuCodee@users.noreply.github.com>
2026-04-07 11:39:35 +0530

    main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,6 +4,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
+        int[][] matrix = new int[n][n];
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        // Fill matrix in spiral order
+        while (top <= bottom && left <= right) {
+
+            // top row
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            // right column
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            // bottom row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        // Print matrix
+        int diagonalSum = 0;
+        for (int i = 0; i < n; i++) {
+            diagonalSum += matrix[i][i];
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j]);
+                if (j < n - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("Diagonal: " + diagonalSum);
+    }
+}
 
         // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
         //       Print each row with values separated by single space
@@ -16,5 +71,4 @@ public class Main {
         // 7 6 5
         // Diagonal: 15
 
-    }
-}
+    
