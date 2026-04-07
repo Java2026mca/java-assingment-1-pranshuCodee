ae6b7632d62c97cd44ead90c05dcbf0ddcedfb96
pranshuCodee <159226022+pranshuCodee@users.noreply.github.com>
2026-04-07 14:21:32 +0530

    main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-    // Check prime
+    
     public static boolean isPrime(int n) {
         if (n <= 1) return false;
         for (int i = 2; i * i <= n; i++) {
@@ -13,7 +13,7 @@ public class Main {
         return true;
     }
 
-    // Check perfect
+    
     public static boolean isPerfect(int n) {
         if (n <= 1) return false;
         int sum = 1;
