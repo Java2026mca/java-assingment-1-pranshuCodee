8871f87fe11c5e00d73536b9369599278a436837
pranshuCodee <159226022+pranshuCodee@users.noreply.github.com>
2026-04-07 11:43:51 +0530

    main.java
    Added methods to check if a number is prime and perfect.

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -4,6 +4,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+    // Check prime
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    // Check perfect
+    public static boolean isPerfect(int n) {
+        if (n <= 1) return false;
+        int sum = 1;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i != n / i) {
+                    sum += n / i;
+                }
+            }
+        }
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            boolean prime = isPrime(num);
+            boolean perfect = isPerfect(num);
+
+            if (prime && perfect) {
+                System.out.println("Both");
+            } else if (prime) {
+                System.out.println("Prime");
+            } else if (perfect) {
+                System.out.println("Perfect");
+            } else {
+                System.out.println("Neither");
+            }
+        }
+    }
+}
 
         // TODO: Read n integers. For each number print:
         //   "Prime"     if it is prime
@@ -22,4 +69,3 @@ public class Main {
         // Perfect
 
     }
-}
