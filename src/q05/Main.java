88bdc9af79ce372772ccfc790e297fb55448ff93
pranshuCodee <159226022+pranshuCodee@users.noreply.github.com>
2026-04-08 07:44:04 +0530

    main.java

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -5,6 +5,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String shape = sc.next();
 
+        double area = 0.0;
+        
+        if (shape.equals("circle")) {
+            double r = sc.nextDouble();
+            area = Math.PI * r * r;
+        } 
+        else if (shape.equals("rectangle")) {
+            double l = sc.nextDouble();
+            double w = sc.nextDouble();
+            area = l * w;
+        } 
+        else if (shape.equals("triangle")) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+            double s = (a + b + c) / 2.0;
+            area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        
+        System.out.printf("Area: %.2f", area);
+    }
+}
+
         // TODO: Compute area using the correct formula
         //
         //   circle    r         → area = π * r * r          (use Math.PI)
