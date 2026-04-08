373779776120c180fc67a36e518ad76f97e15585
pranshuCodee <159226022+pranshuCodee@users.noreply.github.com>
2026-04-08 08:30:33 +0530

    Main. java

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -1,9 +1,41 @@
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
+        String line = sc.nextLine();
+        String[] tokens = line.split(" ");
+        
+        // Array-based stack
+        int[] stack = new int[tokens.length];
+        int top = -1;
+        
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || 
+                token.equals("*") || token.equals("/")) {
+                
+                int b = stack[top--];
+                int a = stack[top--];
+                int result = 0;
+                
+                switch (token) {
+                    case "+": result = a + b; break;
+                    case "-": result = a - b; break;
+                    case "*": result = a * b; break;
+                    case "/": result = a / b; break;
+                }
+                
+                stack[++top] = result;
+            } else {
+                stack[++top] = Integer.parseInt(token);
+            }
+        }
+        
+        System.out.println(stack[top]);
+    }
+}
         // TODO: Evaluate a postfix (Reverse Polish Notation) expression
         //       Operands are non-negative integers, operators are: + - * /
         //       Use a stack to evaluate
@@ -15,7 +47,4 @@ public class Main {
         //
         // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
 
-        String line = sc.nextLine();
-
-    }
-}
+        
